7fb8f1f538769e157de20d147a3bb6395fabd29a
Youssef Kashef <youssef.kashef@gmail.com>
2015-08-03 16:42:02 +0200

    remove net surgery lines

--- pascal_context.py
+++ pascal_context.py
@@ -10,7 +10,7 @@ import fileSystemUtils as fs
 import cv2 as cv2
 import cv2.cv as cv
 
-CAFFE_ROOT = '/home/kashefy/src/caffe_forks/bvlc/'
+CAFFE_ROOT = '/home/kashefy/src/caffe/'
 if CAFFE_ROOT is not None:
     import sys
     sys.path.insert(0,  os.path.join(CAFFE_ROOT, 'python'))
@@ -19,15 +19,8 @@ import caffe
 def main(args):
     
     caffe.set_mode_cpu()
-    solver = caffe.SGDSolver('/media/win/Users/woodstock/dev/data/models/fcn_segm/solver2.prototxt')
     
-    niter = 20
     
-    # the main solver loop
-    for it in range(niter):
-        solver.step(1)  # SGD by Caffe
-        
-    return 0
 
 if __name__ == '__main__':
     
