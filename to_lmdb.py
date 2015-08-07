032890270ee1cf7983ff612eac087491f4ce9d72
Youssef Kashef <youssef.kashef@gmail.com>
2015-08-07 11:49:04 +0200

    remove dead code

--- to_lmdb.py
+++ to_lmdb.py
@@ -6,8 +6,6 @@ Created on Jul 18, 2015
 import os
 import numpy as np
 from scipy import io
-import cv2 as cv2
-import cv2.cv as cv
 import lmdb
 from read_img import read_img_cv2
 
@@ -16,7 +14,6 @@ def imgs_to_lmdb(paths_src, path_dst, CAFFE_ROOT=None):
     Generate LMDB file from set of images
     Source: https://github.com/BVLC/caffe/issues/1698#issuecomment-70211045
     credit: Evan Shelhamer
-    
     '''
     if CAFFE_ROOT is not None:
         import sys
@@ -30,7 +27,9 @@ def imgs_to_lmdb(paths_src, path_dst, CAFFE_ROOT=None):
         for idx, path_ in enumerate(paths_src):
             
             img = read_img_cv2(path_)
-            
+            print path_
+            print img.shape
+            print img
             img_dat = caffe.io.array_to_datum(img)
             in_txn.put('{:0>10d}'.format(idx), img_dat.SerializeToString())
     
@@ -64,7 +63,8 @@ def matfiles_to_lmdb(paths_src, path_dst, fieldname,
             
             if lut is not None:
                 content_field = lut(content_field)
-                
+            print path_
+            print content_field
             img_dat = caffe.io.array_to_datum(content_field)
             in_txn.put('{:0>10d}'.format(idx), img_dat.SerializeToString())
     
@@ -72,20 +72,3 @@ def matfiles_to_lmdb(paths_src, path_dst, fieldname,
 
     return 0
 
-def gen_net(lmdb, batch_size, CAFFE_ROOT):
-    '''
-    For lmdb data inspection
-    '''
-    if CAFFE_ROOT is not None:
-        os.chdir(CAFFE_ROOT)
-        import sys
-        sys.path.insert(0, './python')
-    import caffe
-    from caffe import layers as L
-    from caffe import params as P
-    
-    # our version of LeNet: a series of linear and simple nonlinear transformations
-    n = caffe.NetSpec()
-    n.data, n.label = L.Data(batch_size=batch_size, backend=P.Data.LMDB, source=lmdb,
-                             transform_param=dict(scale=1./255), ntop=2)
-    return n.to_proto()
