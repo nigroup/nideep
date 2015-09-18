94d9bc44e898efb51a62d071e644cdcfcc6752b4
Youssef Kashef <youssef.kashef@gmail.com>
2015-09-18 16:35:18 +0200

    write list of scalar valus to lmdb

--- to_lmdb.py
+++ to_lmdb.py
@@ -68,3 +68,39 @@ def matfiles_to_lmdb(paths_src, path_dst, fieldname,
 
     return 0
 
+def scalars_to_lmdb(scalars, path_dst,
+                    CAFFE_ROOT=None,
+                    lut=None):
+    '''
+    Generate LMDB file from set of images
+    Source: https://github.com/BVLC/caffe/issues/1698#issuecomment-70211045
+    credit: Evan Shelhamer
+    
+    '''
+    if CAFFE_ROOT is not None:
+        import sys
+        sys.path.insert(0,  os.path.join(CAFFE_ROOT, 'python'))
+    import caffe
+    
+    db = lmdb.open(path_dst, map_size=int(1e12))
+    
+    with db.begin(write=True) as in_txn:
+    
+        for idx, x in enumerate(scalars):
+            
+            content_field = np.array([x])
+            # get shape (1,1,1)
+            content_field = np.expand_dims(content_field, axis=0)
+            content_field = np.expand_dims(content_field, axis=0)
+            content_field = content_field.astype(int)
+            
+            if lut is not None:
+                content_field = lut(content_field)
+                
+            img_dat = caffe.io.array_to_datum(content_field)
+            in_txn.put('{:0>10d}'.format(idx), img_dat.SerializeToString())
+    
+    db.close()
+
+    return 0
+
