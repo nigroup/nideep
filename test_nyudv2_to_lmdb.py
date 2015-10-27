480c0080cc19e0381595e46c7484c0a2a5f6333c
Youssef Kashef <youssef.kashef@gmail.com>
2015-10-27 17:59:08 +0100

    validate val list

--- test_nyudv2_to_lmdb.py
+++ test_nyudv2_to_lmdb.py
@@ -1,7 +1,9 @@
-from nose.tools import assert_raises
+from nose.tools import assert_is_instance, assert_list_equal, assert_raises
 import os
 import tempfile
 import shutil
+import numpy as np
+from scipy import io
 import nyudv2_to_lmdb as n2l
     
 class TestHandlingSplitsFile:
@@ -10,6 +12,10 @@ class TestHandlingSplitsFile:
     def setup_class(self):
         
         self.path_temp_dir = tempfile.mkdtemp()
+        self.path_splits = os.path.join(self.path_temp_dir, 'foo.mat')
+        
+        data = np.array([[2], [4], [10]])
+        io.savemat(self.path_splits, data, oned_as='column')
         
     @classmethod
     def teardown_class(self):
@@ -23,3 +29,10 @@ class TestHandlingSplitsFile:
     def test_invalid_path(self):
         
         assert_raises(IOError, n2l.split_matfile_to_val_list, '/foo/bar.mat')
+        
+    def test_val_list(self):
+        
+        val_list = n2l.split_matfile_to_val_list(self.path_splits)
+        assert_is_instance(val_list, list)
+        assert_list_equal(val_list, [[2], [4], [10]])
+        
