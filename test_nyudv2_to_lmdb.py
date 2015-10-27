0170b8a4b8a1da712291b04ae7bb8df020d115df
Youssef Kashef <youssef.kashef@gmail.com>
2015-10-27 17:52:26 +0100

    cleanup tests

--- test_nyudv2_to_lmdb.py
+++ test_nyudv2_to_lmdb.py
@@ -1,12 +1,25 @@
-from nose.tools import assert_is_none
-import nyudv2_to_lmdb as n2l
-
+from nose.tools import assert_raises
 import os
-
-def split_matfile_to_val_list_invalid_path_dir():
+import tempfile
+import shutil
+import nyudv2_to_lmdb as n2l
     
-    n2l.split_matfile_to_val_list(os.curdir)
+class TestHandlingSplitsFile:
     
-def split_matfile_to_val_list_invalid_path():
-    
-    n2l.split_matfile_to_val_list('/foo/bar.mat')
+    @classmethod
+    def setup_class(self):
+        
+        self.path_temp_dir = tempfile.mkdtemp()
+        
+    @classmethod
+    def teardown_class(self):
+        
+        shutil.rmtree(self.path_temp_dir)
+        
+    def test_invalid_path_dir(self):
+        
+        assert_raises(IOError, n2l.split_matfile_to_val_list, os.curdir)
+        
+    def test_invalid_path(self):
+        
+        assert_raises(IOError, n2l.split_matfile_to_val_list, '/foo/bar.mat')
