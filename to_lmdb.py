220274551ce6e788b704dfddf623b511442d3055
Youssef Kashef <youssef.kashef@gmail.com>
2015-10-29 15:43:12 +0100

    save list of arrays to lmdb

--- to_lmdb.py
+++ to_lmdb.py
@@ -38,7 +38,7 @@ def matfiles_to_lmdb(paths_src, path_dst, fieldname,
                      CAFFE_ROOT=None,
                      lut=None):
     '''
-    Generate LMDB file from set of images
+    Generate LMDB file from set of mat files
     Source: https://github.com/BVLC/caffe/issues/1698#issuecomment-70211045
     credit: Evan Shelhamer
     
@@ -72,14 +72,8 @@ def scalars_to_lmdb(scalars, path_dst,
                     CAFFE_ROOT=None,
                     lut=None):
     '''
-    Generate LMDB file from set of images
-    Source: https://github.com/BVLC/caffe/issues/1698#issuecomment-70211045
-    credit: Evan Shelhamer
-    
+    Generate LMDB file from list of scalars
     '''
-    if CAFFE_ROOT is not None:
-        import sys
-        sys.path.insert(0,  os.path.join(CAFFE_ROOT, 'python'))
     import caffe
     
     db = lmdb.open(path_dst, map_size=int(1e12))
@@ -90,8 +84,8 @@ def scalars_to_lmdb(scalars, path_dst,
             
             content_field = np.array([x])
             # get shape (1,1,1)
-            content_field = np.expand_dims(content_field, axis=0)
-            content_field = np.expand_dims(content_field, axis=0)
+            while len(content_field.shape) < 3:
+                content_field = np.expand_dims(content_field, axis=0)
             content_field = content_field.astype(int)
             
             if lut is not None:
@@ -99,6 +93,29 @@ def scalars_to_lmdb(scalars, path_dst,
                 
             img_dat = caffe.io.array_to_datum(content_field)
             in_txn.put('{:0>10d}'.format(idx), img_dat.SerializeToString())
+    
+    db.close()
+
+    return 0
+
+def arrays_to_lmdb(arrs, path_dst):
+    '''
+    Generate LMDB file from list of ndarrays    
+    '''
+    import caffe
+    
+    db = lmdb.open(path_dst, map_size=int(1e12))
+    
+    with db.begin(write=True) as in_txn:
+    
+        for idx, x in enumerate(arrs):
+            
+            content_field = x
+            while len(content_field.shape) < 3:
+                content_field = np.expand_dims(content_field, axis=0)
+            
+            dat = caffe.io.array_to_datum(content_field)
+            in_txn.put('{:0>10d}'.format(idx), dat.SerializeToString())
     
     db.close()
 
