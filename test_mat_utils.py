7c5b7654a6b635475b5ab786c55f1e249c25b728
Youssef Kashef <youssef.kashef@gmail.com>
2015-10-29 12:20:57 +0100

    add test around cwh_to_chw

--- test_mat_utils.py
+++ test_mat_utils.py
@@ -15,6 +15,39 @@ class TestTranspose:
         assert_raises(AttributeError, mu.cwh_to_chw, np.random.rand(3, 2))
         assert_raises(AttributeError, mu.cwh_to_chw, np.random.rand(5, 4, 3, 2))
         
+    def test_cwh_to_chw(self):
+        
+        x = np.array([[[ 1,  2,  3],
+                       [ 4,  5,  6]
+                       ],
+                      [[ 7,  8,  9],
+                       [10, 11, 12]
+                       ],
+                      [[13, 14, 15],
+                       [16, 17, 18],
+                       ],
+                      [[19, 20, 21],
+                       [22, 23, 24]
+                       ]
+                      ])
+        
+        assert_equals(x.shape[0], 4) # c
+        assert_equals(x.shape[1], 2) # w
+        assert_equals(x.shape[2], 3) # h
+        
+        y = mu.cwh_to_chw(x)
+        
+        assert_equals(y.shape[0], 4)
+        assert_equals(y.shape[1], 3)
+        assert_equals(y.shape[2], 2)
+        
+        assert_equals(x[3][1][2], y[3][2][1])
+        
+        for i in range(4):
+            for j in range(2):
+                for k in range(3):
+                    assert_equals(x[i][j][k], y[i][k][j])
+        
     def test_hwc_to_chw_invalid_dims(self):
         
         assert_raises(AttributeError, mu.hwc_to_chw, np.random.rand(3))
@@ -37,9 +70,9 @@ class TestTranspose:
                        ]
                       ])
         
-        assert_equals(x.shape[0], 4)
-        assert_equals(x.shape[1], 2)
-        assert_equals(x.shape[2], 3)
+        assert_equals(x.shape[0], 4) # h
+        assert_equals(x.shape[1], 2) # w
+        assert_equals(x.shape[2], 3) # c
         
         y = mu.hwc_to_chw(x)
         
