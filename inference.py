6f2779aa80722c1dc2cfed76951ef1434c681c4c
Youssef Kashef <youssef.kashef@gmail.com>
2015-12-15 18:57:36 +0100

    inference output to lmdb

--- inference.py
+++ inference.py
@@ -6,6 +6,8 @@ Created on Dec 3, 2015
 import numpy as np
 import h5py
 import caffe
+import read_lmdb
+import to_lmdb
 
 def infer_to_h5_fixed_dims(net, keys, n, dst_fpath, preserve_batch=False):
     """
@@ -26,6 +28,24 @@ def infer_to_h5_fixed_dims(net, keys, n, dst_fpath, preserve_batch=False):
             
     return [len(dc[k]) for k in keys]
 
+def infer_to_lmdb(net, keys, n, dst_fpath, preserve_batch=False):
+    """
+    Run network inference for n batches and save results to file
+    """
+    dc = {k:[] for k in keys}
+    for _ in range(n):
+        d = forward(net, keys)
+        for k in keys:
+            if preserve_batch:
+                dc[k].append(np.copy(d[k].astype(float)))
+            else:
+                dc[k].extend(np.copy(d[k].astype(float)))
+          
+    for k in keys:
+        to_lmdb.arrays_to_lmdb(dc[k], dst_fpath)
+            
+    return [len(dc[k]) for k in keys]
+
 def forward(net, keys):
     
     net.forward()
@@ -33,12 +53,30 @@ def forward(net, keys):
 
 if __name__ == '__main__':
     
-    import os
-    fpath_net = os.path.expanduser('~/models/dark/mnist/t0/lenet_train_test.prototxt')
-    fpath_weights = os.path.expanduser('~/models/dark/mnist/t0/lenet_iter_10000.caffemodel')
+    from os.path import expanduser
+        
+    fpath_net = expanduser('~/models/dark/mnist/t0/lenet_train_test_inf.prototxt')
+    
+    fpath_weights = expanduser('~/models/dark/mnist/t0/lenet_iter_10000.caffemodel')
+    net = caffe.Net(fpath_net, fpath_weights, caffe.TRAIN)
+    
+    fpath = expanduser('~/models/dark/mnist/t0/mnist_dark0_sm_train')
+    num_entries = read_lmdb.num_entries("/home/kashefy/data/mnist/mnist_train_lmdb")
+
+    
+    #infer_to_h5_fixed_dims(net, ['prob'], num_entries, "%s.h5" % fpath)
+    infer_to_lmdb(net, ['prob'], num_entries, "%s_lmdb" % fpath)
+    
     net = caffe.Net(fpath_net, fpath_weights, caffe.TEST)
-    fpath = os.path.expanduser('~/models/dark/mnist/t0/x.h5')
-    infer_to_h5_fixed_dims(net, ['data', 'label'], 4, fpath)
+    fpath = expanduser('~/models/dark/mnist/t0/mnist_dark0_sm_test')
+    num_entries = read_lmdb.num_entries("/home/kashefy/data/mnist/mnist_test_lmdb")
+    #infer_to_h5_fixed_dims(net, ['prob'], num_entries, "%s.h5" % fpath)
+    infer_to_lmdb(net, ['prob'], num_entries, "%s_lmdb" % fpath)
+    
+    print 'done'
+    
+    #l = read_lmdb.read_labels('/home/kashefy/data/mnist/mnist_train_lmdb')
+    #p = [x for x, _ in read_lmdb.read_values("~/models/dark/mnist/t0/mnist_dark0_sm_train_lmdb")]
     
     
 #     with h5py.File(fpath, "w") as f:
