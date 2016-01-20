4065df105c83e13a8ac27e4dffa03fa2d00dc5a5
Youssef Kashef <youssef.kashef@gmail.com>
2016-01-20 15:42:09 +0100

    test wrapper function to save list of arrays to hdf5

--- to_hdf5.py
+++ to_hdf5.py
@@ -7,11 +7,9 @@ import h5py
 from mat_utils import expand_dims
 
 def arrays_to_h5_fixed(arrs, key, path_dst):
-    """
-    """        
+    '''
+    save list of arrays (all same size) to hdf5 under a single key
+    '''
     with h5py.File(path_dst, "w") as f:
-        
-        for x in arrs:
-            content_field = expand_dims(x, 3)
-            f[key] = content_field
+        f[key] = [expand_dims(x, 3) for x in arrs]
             
