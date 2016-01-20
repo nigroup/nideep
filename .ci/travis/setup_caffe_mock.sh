fde8db599146ec9c58fba24d300345d6fae042c8
Youssef Kashef <youssef.kashef@gmail.com>
2016-01-20 17:06:58 +0100

    refactor setup script

--- .ci/travis/setup_caffe_mock.sh
+++ .ci/travis/setup_caffe_mock.sh
@@ -7,9 +7,10 @@ if [[ -z "$ROOT_DIR" ]]; then
     exit 1
 fi;
     
-mkdir -p $ROOT_DIR/caffe/proto/
-protoc -I=$ROOT_DIR/ --python_out=$ROOT_DIR/caffe/proto/ $ROOT_DIR/caffe.proto
-
 # For mocking caffe
+mkdir -p $ROOT_DIR/caffe
 mv $ROOT_DIR/caffe_mock.py $ROOT_DIR/caffe/__init__.py
+
+mkdir -$ROOT_DIR/caffe/proto/
+protoc -I=$ROOT_DIR/ --python_out=$ROOT_DIR/caffe/proto/ $ROOT_DIR/caffe.proto
 touch $ROOT_DIR/caffe/proto/__init__.py
