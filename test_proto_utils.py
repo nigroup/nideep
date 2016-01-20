575d7ff7b42761594ba281f46714c350a2825844
Youssef Kashef <youssef.kashef@gmail.com>
2016-01-20 17:45:13 +0100

    preliminary test for parser

--- test_proto_utils.py
+++ test_proto_utils.py
@@ -3,12 +3,18 @@ Created on Jan 20, 2016
 
 @author: kashefy
 '''
-from nose.tools import assert_equal, assert_true, assert_list_equal
+from nose.tools import assert_equal, assert_true, assert_list_equal,\
+    assert_is_not_none
 import os
 import tempfile
 import shutil
 import numpy as np
 import proto_utils as pu
+
+import sys
+CURRENT_MODULE_PATH = os.path.abspath(sys.modules[__name__].__file__)
+TEST_DATA_DIRNAME = 'test_data'
+TEST_NET_FILENAME = 'n1.prototxt'
         
 class TestProtoUtils:
 
@@ -22,6 +28,13 @@ class TestProtoUtils:
         
         shutil.rmtree(self.dir_tmp)
     
-    def test_net_merge(self):
+    def test_from_net_params_file(self):
         
-        assert_true(True)
+        fpath = os.path.join(os.path.dirname(CURRENT_MODULE_PATH),
+                             TEST_DATA_DIRNAME, TEST_NET_FILENAME)
+        
+        parser = pu.Parser()
+        net_spec = parser.from_net_params_file(fpath)
+        
+        assert_is_not_none(net_spec)
+        
