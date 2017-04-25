4ed34b28c67d82e95bab8e3813e033004820a9d7
Youssef Kashef <youssef.kashef@gmail.com>
2017-04-25 23:19:07 +0200

    auto format

--- nideep/datasets/twoears/test_label_utils.py
+++ nideep/datasets/twoears/test_label_utils.py
@@ -13,82 +13,82 @@ from h5py import File
 import label_utils as lu
 
 class TestLabelUtils:
-    
+
     @classmethod
     def setup_class(self):
 
         self.dir_tmp = tempfile.mkdtemp()
-                        
+
     @classmethod
     def teardown_class(self):
         shutil.rmtree(self.dir_tmp)
-        
+
     def test_id_loc_to_loc(self):
-        
+
         p = os.path.join(self.dir_tmp, 'x.h5')
         new_key = 'new_key'
-        fake_data = np.array([[[[  1,  2,  3]],
+        fake_data = np.array([[[[  1, 2, 3]],
                                [[ 10, 20, 30]],
-                               [[100,200,300]]],
-                              [[[  4,  5,  6]],
+                               [[100, 200, 300]]],
+                              [[[  4, 5, 6]],
                                [[ 40, 50, 60]],
-                               [[400,500,600]]]])
+                               [[400, 500, 600]]]])
         with File(p, 'w') as h:
             h['label_id_loc'] = fake_data
             assert_not_in(new_key, h)
             lu.id_loc_to_loc(p, new_key)
             assert_in(new_key, h, "new key missing")
-        
+
         with File(p, 'r') as h:
             x = h[new_key][:]
             assert_equal(x.ndim, fake_data.ndim)
-            assert_equal(x.shape[-1], fake_data.shape[-1]-1, "void bin expected to be eliminated")
+            assert_equal(x.shape[-1], fake_data.shape[-1] - 1, "void bin expected to be eliminated")
             assert_equal(x.shape[0], fake_data.shape[0], "batch bin has changed")
             expected = \
-                np.array([[[[111,222,333]]],
-                          [[[444,555,666]]]])
-            assert_true(np.all(x == expected[...,:-1]))
+                np.array([[[[111, 222, 333]]],
+                          [[[444, 555, 666]]]])
+            assert_true(np.all(x == expected[..., :-1]))
         pass
-    
+
     def test_id_loc_to_loc_incl_void(self):
-        
+
         p = os.path.join(self.dir_tmp, 'x.h5')
         new_key = 'new_key'
-        fake_data = np.array([[[[  1,  2,  3]],
+        fake_data = np.array([[[[  1, 2, 3]],
                                [[ 10, 20, 30]],
-                               [[100,200,300]]],
-                              [[[  4,  5,  6]],
+                               [[100, 200, 300]]],
+                              [[[  4, 5, 6]],
                                [[ 40, 50, 60]],
-                               [[400,500,600]]]])
+                               [[400, 500, 600]]]])
         with File(p, 'w') as h:
             h['label_id_loc'] = fake_data
             assert_not_in(new_key, h)
             lu.id_loc_to_loc(p, new_key, has_void_bin=False)
             assert_in(new_key, h, "new key missing")
-        
+
         with File(p, 'r') as h:
             x = h[new_key][:]
             assert_equal(x.ndim, fake_data.ndim)
             assert_equal(x.shape[-1], fake_data.shape[-1], "void bin expected to be preserved")
             assert_equal(x.shape[0], fake_data.shape[0], "batch bin has changed")
             expected = \
-                np.array([[[[111,222,333]]],
-                          [[[444,555,666]]]])
+                np.array([[[[111, 222, 333]]],
+                          [[[444, 555, 666]]]])
             assert_true(np.all(x == expected))
         pass
-    
+
     def test_id_loc_to_loc_keys(self):
-        
+
         p = os.path.join(self.dir_tmp, 'x.h5')
-        fake_data = np.array([[[[  1,  2,  3]],
+        fake_data = np.array([[[[  1, 2, 3]],
                        [[ 10, 20, 30]],
-                       [[100,200,300]]],
-                      [[[  4,  5,  6]],
+                       [[100, 200, 300]]],
+                      [[[  4, 5, 6]],
                        [[ 40, 50, 60]],
-                       [[400,500,600]]]])
+                       [[400, 500, 600]]]])
         with File(p, 'w') as h:
             h['label_id_loc'] = fake_data
-            
+
             keys = [k.encode('ascii', 'ignore') for k in h.keys()]
             for new_key in ['a', 'b', 'new_key']:
                 assert_not_in(new_key, h)
