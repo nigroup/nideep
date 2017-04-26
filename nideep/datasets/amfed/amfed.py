cf02585a7627824b12d88850216bc308d5656db3
Rodrigo Parra <rodpar07@gmail.com>
2017-04-26 15:03:50 +0200

    Fixes and refactoring

--- nideep/datasets/amfed/amfed.py
+++ nideep/datasets/amfed/amfed.py
@@ -3,17 +3,16 @@ Created on Mar 28, 2017
 
 @author: kashefy
 '''
-import os
 import logging
+import os
 from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+
 import nideep.iow.file_system_utils as fs
 from comparables import list_comparables
-import cv2
-import pandas as pd
-import numpy as np
-from PIL import Image
-import cPickle as pickle
-from preprocessing import crop_face
+from nideep.datasets.amfed.entity import Entity
 
 
 class AMFED(object):
@@ -105,122 +104,22 @@ class AMFED(object):
         common_entities = self.from_list_to_tuple(common_entity_names, lists, list_names)
         return pd.DataFrame([c._asdict() for c in common_entities])
 
-    def as_dataframe(self):
-        index_path = self.cache_dir + AMFED.INDEX_FILE
-        if os.path.isfile(index_path):
-            result_df = pd.read_csv(index_path)
-        else:
-            entities = self.find_data_intersection()
-            count = 0
-            result = []
-            for _, e in entities.iterrows():
-                entity_records = self.load_file(e)
-                result += entity_records
-                print 'Finished video %d/%d' % (count + 1, len(entities))
-                count += 1
-
-            result_df = pd.DataFrame(result)
-            result_df.to_csv(index_path)
-
-        return result_df
-
     def as_numpy_array(self):
-        df = self.as_dataframe()
-        df = df[df.valid]
-        # X_train = np.array([cv2.imread(e, cv2.IMREAD_GRAYSCALE) for e in df['features'].tolist()])
-        X_train = np.array([cv2.imread(e) for e in df['features'].tolist()])
-        y_train = np.array([pickle.load(open(e, 'rb')) for e in df['labels'].tolist()])
-        return X_train, y_train
-
-    def load_file(self, entity):
-        """
-        Loads a specific video of the dataset, preprocess it (frame extraction, resizing) and return it as a numpy array
-        :return: A list of dictionaries
-        """
-        labels_df = pd.read_csv(entity.au_label)
-        landmarks_df = pd.read_csv(entity.landmarks, header=None)
-
-        vidcap = cv2.VideoCapture(entity.video)
-        success, image = vidcap.read()
+        entities = self.find_data_intersection()
         count = 0
-        success = True
         result = []
-
-        while success:
-
-            label_file = self.__labels_filename(entity, count)
-            frame_file = self.__frame_filename(entity, count)
-
-            record = {'features': frame_file, 'labels': label_file, 'message': '', 'valid': True}
-
-            if labels_df.empty:
-                record['valid'] = False
-                record['message'] = 'Empty labels file'
-            else:
-                if not (os.path.isfile(label_file) and os.path.isfile(frame_file)):
-                    success, msg = self.preprocess_frame(entity, image, count, labels_df=labels_df,
-                                                         landmarks_df=landmarks_df)
-                    record['valid'] = success
-                    record['message'] = msg
-
-            result.append(record)
-            success, image = vidcap.read()
+        for _, e in entities.iterrows():
+            entity = Entity(e.video, e.au_label, e.landmarks, self.cache_dir)
+            entity_records = entity.frames(projection=['Smile', 'AU02', 'AU04', 'Trackerfail'], valid_only=True)
+            result += entity_records
+            self.logger.info('Finished video %d/%d' % (count + 1, len(entities)))
             count += 1
 
-        return result
+        X_train = np.concatenate([np.expand_dims(r['features'], axis=0) for r in result])
+        y_train = np.concatenate([np.expand_dims(r['labels'][:-1], axis=0) for r in result])
 
-    def preprocess_frame(self, entity, frame, frame_number, labels_df=None, landmarks_df=None):
-        label_file = self.__labels_filename(entity, frame_number)
-        frame_file = self.__frame_filename(entity, frame_number)
+        return X_train, y_train
 
-        time = AMFED.__frame_to_timestamp(frame_number)
-
-        if labels_df is None:
-            labels_df = pd.read_csv(entity.au_label)
-
-        if not landmarks_df is None:
-            landmarks_df = pd.read_csv(entity.landmarks, header=None)
-
-        labels = []
-        aucs = ['Smile', 'AU02', 'AU04', 'Trackerfail']  # TODO remove hardcoded aucs
-
-        label_columns = [e for e in aucs if e in list(labels_df)]
-        if label_columns:
-            label_row = labels_df[labels_df['Time'] <= time].iloc[-1].loc[label_columns]
-            labels = [1 if e >= 50 else 0 for e in label_row]
-
-        if frame_number < landmarks_df.shape[0]:
-            left_eye = (
-                landmarks_df.iloc[[frame_number]][0][frame_number], landmarks_df.iloc[[frame_number]][1][frame_number])
-            right_eye = (
-                landmarks_df.iloc[[frame_number]][2][frame_number], landmarks_df.iloc[[frame_number]][3][frame_number])
-        else:
-            return False, 'Frame number > number of landmark file rows'
-
-        if not all(v == 0 for v in (left_eye + right_eye)) and len(labels) == len(aucs):
-            # print 'Read a new frame from video: ', count
-
-            if labels[-1]:
-                return False, 'Tracker fail: human labeled failure'
-
-            cropped = np.array(crop_face(Image.fromarray(frame), eye_left=left_eye, eye_right=right_eye,
-                                         offset_pct=(0.25, 0.25), dest_sz=(64, 64)))
-
-            # gray_image = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
-
-            pickle.dump(labels[:-1], open(label_file, 'wb'))
-            cv2.imwrite(frame_file, cropped)  # save frame as JPEG file
-            return True, 'Success'
-        else:
-            return False, 'Tracker fail: no landmarks available'
-
-    def __labels_filename(self, entity, frame_number):
-        video_name = entity.video.split('/')[-1][:-4]
-        return self.cache_dir + video_name + '-labels%d.p' % frame_number
-
-    def __frame_filename(self, entity, frame_number):
-        video_name = entity.video.split('/')[-1][:-4]
-        return self.cache_dir + video_name + '-frame%d.jpg' % frame_number
 
     @staticmethod
     def __timestamp_to_frame(time):
@@ -233,14 +132,13 @@ class AMFED(object):
     def set_subset(self, entities):
         self.entities = entities
         
-    def dense_labels(self):
-        raise NotImplementedError # TODO: Implement
 
     def __init__(self, dir_prefix, video_type=VIDEO_TYPE_FLV, cache_dir=None):
         '''
         Constructor
         '''
         self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO)
         self.cache_dir = cache_dir
         if not os.path.exists(cache_dir):
             os.makedirs(cache_dir)
