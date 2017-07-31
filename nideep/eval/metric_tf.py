ea003011c8fa3bd186f7c4b9cd0a7d50519b6276
Youssef Kashef <youssef.kashef@gmail.com>
2017-07-31 12:59:02 +0200

    test method to test function

--- nideep/eval/metric_tf.py
+++ nideep/eval/metric_tf.py
@@ -5,11 +5,11 @@ Created on Jul 28, 2017
 '''
 import tensorflow as tf
 
-def resettable_metric(metric, scope, **metric_args):
+def resettable_metric(metric, scope_name, **metric_args):
     '''
     Originally from https://github.com/tensorflow/tensorflow/issues/4814#issuecomment-314801758
     '''
-    with tf.variable_scope(scope) as scope:
+    with tf.variable_scope(scope_name) as scope:
         metric_op, update_op = metric(**metric_args)
         v = tf.contrib.framework.get_variables(\
                     scope, collection=tf.GraphKeys.LOCAL_VARIABLES)
