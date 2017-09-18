fix scaling

nideep/datasets/mnist/mnist_tf.py
@@ -41,7 +41,7 @@ class MNIST(object):
         train_idxs = perm0[validation_size:]
         val_idxs = perm0[:validation_size]
         train = DataSet(\
-                    np.multiply(ds.train.images[train_idxs], 255.), # will rescale to [0,1] inside
+                    np.multiply(ds.train.images[train_idxs], [1., 255.][dtype == dtypes.float32]), # will rescale to [0,1] inside
                     ds.train.labels[train_idxs],
                     fake_data=fake_data,
                     one_hot=one_hot,
@@ -49,7 +49,7 @@ class MNIST(object):
                     reshape=False, # already reshaped
                     seed=seed)
         validation = DataSet(\
-                        np.multiply(ds.train.images[val_idxs], 255.), # will rescale to [0,1] inside
+                        np.multiply(ds.train.images[val_idxs], [1., 255.][dtype == dtypes.float32]), # will rescale to [0,1] inside
                         ds.train.labels[val_idxs],
                         fake_data=fake_data,
                         one_hot=one_hot,