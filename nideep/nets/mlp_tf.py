01d064cb3a19628a04526c81c7f3d17e1284b6e3
Youssef Kashef <youssef.kashef@gmail.com>
2017-11-07 22:14:58 +0100

    WIP: second task

--- nideep/nets/mlp_tf.py
+++ nideep/nets/mlp_tf.py
@@ -5,7 +5,6 @@ Created on Jul 14, 2017
 '''
 import tensorflow as tf
 from nideep.nets.abstract_net_tf import AbstractNetTF
-from networkx.algorithms.shortest_paths.unweighted import predecessor
 
 class MLP(AbstractNetTF):
     '''
@@ -22,6 +21,12 @@ class MLP(AbstractNetTF):
                                                 [input_dim, dim],
                                                 initializer=self._init_weight_op()
                                                 )
+            if idx == len(self.n_nodes)-1:
+                fc_name_w = 'fc-%d-aux/w' % idx
+                self.w[fc_name_w] = tf.get_variable(fc_name_w,
+                                                    [input_dim, 9],
+                                                    initializer=self._init_weight_op()
+                                                    )
         self._init_bias_vars(bias_value=0.1)
                 
     def _fc(self, x):
@@ -38,6 +43,18 @@ class MLP(AbstractNetTF):
             else:
                 self._y_logits = fc_op
                 self.p = tf.nn.softmax(fc_op, name='a-%d' % idx)
+        
+        fc_name_w = 'fc-%d-aux/w' % idx
+        fc_name_b = 'fc-%d-aux/b' % idx
+        if len(self.n_nodes) == 1:
+            in_op = x
+        else:
+            in_op = a
+        fc_op = tf.add(tf.matmul(in_op, self.w[fc_name_w]),
+                        self.b[fc_name_b],
+                        name='fc-%d' % idx)
+        self._y_logits_aux = fc_op
+        self.p_aux = tf.nn.softmax(fc_op, name='a-%d-aux' % idx)
         return self.p, self._y_logits
             
     def _init_ops(self):
@@ -46,11 +63,25 @@ class MLP(AbstractNetTF):
                 
     def cost(self, y_true, name=None):
         with tf.name_scope(self.name_scope):
-            self._cost_op = tf.reduce_mean(
+            c = tf.reduce_mean(
                 tf.nn.softmax_cross_entropy_with_logits(\
                         labels=y_true, logits=self._y_logits),
                         name=name)
-            return self._cost_op
+            self._cost_ops.append(c)
+            return c
+        
+    def cost_o(self, y_true, name=None):
+        with tf.name_scope(self.name_scope):
+            c = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(\
+                        labels=y_true, logits=self._y_logits_aux),
+                        name=name)
+            self._cost_ops.append(c)
+            return c
+        
+    @property
+    def cost_ops(self):
+        return self._cost_ops
         
     def __init__(self, params):
         '''
@@ -61,5 +92,5 @@ class MLP(AbstractNetTF):
         self.n_nodes = params['n_nodes']  # 1st layer num features
         super(MLP, self).__init__(params)
 
-        self._cost_op = None
+        self._cost_ops = []
         self._y_logits = None
